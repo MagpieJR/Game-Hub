554f816c4392e3f0bfd2970574e6fd610e804792
MagpieR <amlugion@gmail.com>
2023-09-26 16:13:44 +0800

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,11 +13,12 @@ export interface Platform {
 }
 
 export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
+  id: number,
+  name: string,
+  background_image: string,
+  parent_platforms: { platform: Platform }[],
+  metacritic: number,
+  rating_top: number,
 }
 
 const useGames = (
