c47cb70b2a097bbe360579ecff7777312b2052a9
MagpieR <amlugion@gmail.com>
2023-09-26 15:41:08 +0800

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,21 @@
-import { HStack, Image, List, ListItem, Spinner, Button, Box } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Button,
+  Box,
+  Heading,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreSkeleton from "./GenreSkeleton";
 import { skeletons } from "./GameGrid";
 
-interface Props{
-  onSelectedGenre: (genre: Genre) => void,
-  selectedGenre: Genre | null,
+interface Props {
+  onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
 const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
@@ -16,26 +25,40 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   //if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <ListItem key={skeleton} paddingY='10px'>
-            <GenreSkeleton />
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <List>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <ListItem key={skeleton} paddingY="10px">
+              <GenreSkeleton />
+            </ListItem>
+          ))}
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                objectFit="cover"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                whiteSpace="normal"
+                textAlign="left"
+                variant="link"
+                onClick={() => onSelectedGenre(genre)}
+                fontSize="lg"
+              >
+                {" "}
+                {genre.name}{" "}
+              </Button>
+            </HStack>
           </ListItem>
         ))}
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Box fontWeight={ genre.id === selectedGenre?.id ? 'bold' : 'normal'} as='button' display='flex' flex-wrap='wrap' onClick={() => onSelectedGenre(genre)} fontSize="lg" > {genre.name} </Box>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+      </List>
+    </>
   );
 };
 
